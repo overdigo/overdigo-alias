5986bfd31c9b846d09f91ff5b7d8c4d6381a2cc2
Rodrigo <overdigo@gmail.com>
2019-01-21 12:40:13 +0000

    Update overdigo-alias.sh

--- overdigo-alias.sh
+++ overdigo-alias.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd ~/
+cd $HOME
 wget https://raw.githubusercontent.com/overdigo/overdigo-alias/master/alias
-cat alias > .bashrc
-source .bashrc
+cat alias > $HOME/.bashrc
+source $HOME/.bashrc
 rm alias
